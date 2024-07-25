8241875a606fdf6a588402448ff20d72f4bdc13e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-25 16:29:15 -0400

    Refactor navigation and routing structure
    Simplified imports and updated routing components to reflect new navigation and page structure. Removed unused navigation code and modularized navigation logic through a dedicated component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,82 +1,54 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import $ from 'jquery'
-import {Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, NextUIProvider} from "@nextui-org/react";
+import React from "react";
+import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import $ from "jquery";
+import {NextUIProvider} from "@nextui-org/react";
 
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
+import "./assets/scss/index.scss";
+import {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
+import Navigation from "./assets/components/Navigation.tsx";
+import LoginPage from "./assets/pages/LoginPage.tsx";
+import StoresPage from "./assets/pages/StoresPage.tsx";
+import Stores from "./assets/ts/stores.ts";
+import DepartmentsPage from "./assets/pages/DepartmentsPage.tsx";
 
-import "./assets/scss/index.scss"
-import Home from "./assets/pages/Home.tsx";
-import About from "./assets/pages/About.tsx";
-import ThemeSwitcher, {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
-
-applyTheme()
+applyTheme();
 
 ReactDOM.createRoot($("#root")[0]!).render(
     <React.StrictMode>
         <BrowserRouter>
             <PageContent/>
         </BrowserRouter>
-    </React.StrictMode>,
-)
+    </React.StrictMode>
+);
 
 
-function PageContent() {
+function PageContent()
+{
     const navigate = useNavigate();
+    // const auth = new Authentication();
+    // auth.loginWithTokenFromCookie().then(res =>
+    // {
+    //     console.log(`logged in`, res);
+    //     if (window.location.pathname !== "/login" && !res)
+    //     {
+    //         navigate("/login");
+    //     }
+    // });
+    if (!Stores.hasStores())
+        Stores.init();
     return (
         <NextUIProvider navigate={navigate}>
-            <Nav/>
+            <Navigation/>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
+                    <Route path="/" element={<></>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/stores" element={<StoresPage/>}/>
+                    <Route path="/stores/:store" element={<DepartmentsPage/>}/>
                 </Route>
             </Routes>
         </NextUIProvider>
     );
 }
 
-
-function Nav() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const pages = {
-        "Home": "/",
-        "About": "/about",
-    };
-    const menuItems = Object.keys(pages).map((item, index) => {
-        const url = Object.values(pages)[index];
-        const isCurrentPage = window.location.pathname === url;
-        return (
-            <NavbarMenuItem>
-                <Link href={url} color={isCurrentPage ? "primary" : "foreground"} aria-current="page" size="lg" className="w-full">
-                    {item}
-                </Link>
-            </NavbarMenuItem>
-        );
-    });
-
-
-    return (
-        <Navbar onMenuOpenChange={setIsMenuOpen}>
-            <NavbarContent>
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">Example</p>
-                </NavbarBrand>
-            </NavbarContent>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                {menuItems}
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    <ThemeSwitcher/>
-                </NavbarItem>
-            </NavbarContent>
-
-            <NavbarMenu>
-                {menuItems}
-            </NavbarMenu>
-        </Navbar>)
-}
