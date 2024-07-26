09dafa33ae51703f239eac3d57e612ac56c87b32
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-26 13:13:43 -0400

    Adjust heading size and update image resolution in StoresPage
    Modified the heading to be responsive, changing sizes based on screen width. Updated the image resolution in the ImageCard component for better quality display.

--- src/assets/pages/StoresPage.tsx
+++ src/assets/pages/StoresPage.tsx
@@ -7,7 +7,7 @@ export default function StoresPage()
     const navigate = useNavigate();
     return (
         <>
-            <h1 className={"text-9xl ml-9"}>Stores</h1>
+            <h1 className={"text-2xl ml-9 mb-4 sm:text-4xl md:text-6xl lg:text-9xl"}>Stores</h1>
             <div className={"flex flex-wrap flex-grow"}>
                 {
                     Stores.getStores().map((store, index) =>
@@ -15,7 +15,7 @@ export default function StoresPage()
                         return (
                             <ImageCard
                                 key={index}
-                                image={store.images["992"]}
+                                image={store.images["150"]}
                                 title={store.name}
                                 description={store.address}
                                 className={"flex-grow flex-shrink"}
