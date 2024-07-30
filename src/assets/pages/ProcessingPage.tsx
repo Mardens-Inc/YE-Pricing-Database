e4c18a3f04662d34a96d1405832e6aac452b4869
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-30 16:29:17 -0400

    Add inputs and buttons to ProcessingPage
    Enhanced interface on ProcessingPage by including various Input components and a Button for adding items. Also integrated DatabaseListComponent for displaying database items relevant to the selected store and department.

--- src/assets/pages/ProcessingPage.tsx
+++ src/assets/pages/ProcessingPage.tsx
@@ -1,5 +1,8 @@
 import {useNavigate, useParams} from "react-router-dom";
 import Stores from "../ts/stores.ts";
+import DatabaseListComponent from "../components/DatabaseListComponent.tsx";
+import {Button, Input, Textarea} from "@nextui-org/react";
+import {Employee} from "../ts/useEmployeeList.ts";
 
 export default function ProcessingPage()
 {
@@ -14,14 +17,38 @@ export default function ProcessingPage()
         return <></>;
     }
 
+    if (departmentName === undefined)
+    {
+        navigate(`/stores/${storeName}`);
+        return <></>;
+    }
+
     const store = Stores.getStores().filter(store => store.name.toLowerCase() === storeName.toLowerCase())[0];
 
     return (
         <>
-            <h1 className={"text-2xl ml-9 mb-4 sm:text-2xl md:text-4xl lg:text-7xl capitalize"}>{departmentName}</h1>
-            <h2 className={"text-2xl ml-9 mb-4 sm:text-2xl md:text-3xl lg:text-4xl"}>{store.name}</h2>
-            <div className={"flex flex-wrap flex-grow"}>
+            <div className={"flex flex-row items-end"}>
+                <h1 className={"text-2xl ml-9 mb-4 sm:text-4xl md:text-5xl lg:text-7xl capitalize"}>{departmentName}</h1>
+                <h2 className={"text-2xl ml-9 mb-4 sm:text-3xl md:text-4xl lg:text-6xl opacity-70 italic"}>{store.name}</h2>
             </div>
+
+            <div className={"w-[90%] mx-auto my-10 flex flex-row gap-3"}>
+                <div className={"flex flex-col gap-3 w-[25%]"}>
+                    <Input label={"Tag Number"}/>
+                    <Textarea label={"Description"}/>
+                </div>
+                <div className={"flex flex-col gap-3 w-[75%]"}>
+                    <Input label={"Percent"} type={"number"}/>
+                    <div className={"flex flex-row gap-3"}>
+                        <Input label={"Mardens Price or Tag Price"} type={"number"}/>
+                        <Input label={"Quantity"} className={"w-[25%]"} type={"number"}/>
+                    </div>
+                    <Button radius={"full"}>Add</Button>
+                </div>
+            </div>
+
+
+            <DatabaseListComponent store={storeName} department={departmentName!} employee={JSON.parse(window.localStorage.getItem("employee")!) as Employee}/>
         </>
     );
 }
