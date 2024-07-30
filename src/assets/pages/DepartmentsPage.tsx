bb1dcdc5f2cee9058d96f56af0ede4e57ae3d7af
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-30 16:28:35 -0400

    Refactor and improve department navigation logic
    Refactored the department list into a globally accessible constant. Improved navigation by handling null store names and updating local storage when navigating to a department.

--- src/assets/pages/DepartmentsPage.tsx
+++ src/assets/pages/DepartmentsPage.tsx
@@ -10,6 +10,7 @@ import flooring from "/src/assets/images/flooring.webp";
 import fabric from "/src/assets/images/fabric.webp";
 import general from "/src/assets/images/general.webp";
 import food from "/src/assets/images/food.webp";
+import StoresPage from "./StoresPage.tsx";
 
 
 interface Department
@@ -18,41 +19,47 @@ interface Department
     image: string,
 }
 
+export const all_departments: Department[] = [
+    {name: "Clothing", image: clothing},
+    {name: "Shoes", image: shoes},
+    {name: "Hardware", image: hardware},
+    {name: "Furniture", image: furniture},
+    {name: "Flooring", image: flooring},
+    {name: "Fabric", image: fabric},
+    {name: "General", image: general},
+    {name: "Food", image: food}
+];
+
 export default function DepartmentsPage()
 {
+    const storeName: string | undefined | null = useParams().store;
+    console.log(storeName == "null");
     const navigate = useNavigate();
-    const storeName: string | undefined = useParams().store;
-    if (storeName === undefined)
+    if (storeName === undefined || storeName === "null")
     {
-        navigate("/stores");
-        return <></>;
+        navigate("/stores/");
+        return <StoresPage/>;
     }
 
     const store = Stores.getStores().filter(store => store.name.toLowerCase() === storeName.toLowerCase())[0];
-    const department: Department[] = [
-        {name: "Clothing", image: clothing},
-        {name: "Shoes", image: shoes},
-        {name: "Hardware", image: hardware},
-        {name: "Furniture", image: furniture},
-        {name: "Flooring", image: flooring},
-        {name: "Fabric", image: fabric},
-        {name: "General", image: general},
-        {name: "Food", image: food}
-    ];
 
     return (
         <>
             <h1 className={"text-2xl ml-9 mb-4 sm:text-4xl md:text-6xl lg:text-9xl"}>Departments for {store.name}</h1>
             <div className={"flex flex-wrap flex-grow"}>
                 {
-                    department.map(dept =>
+                    all_departments.map(dept =>
                     {
                         return (
                             <ImageCard
                                 image={dept.image}
                                 title={dept.name}
                                 className={"flex-grow flex-shrink"}
-                                onClick={() => navigate(`/stores/${store.name.toLowerCase()}/${dept.name.toLowerCase()}`)}
+                                onClick={() =>
+                                {
+                                    localStorage.setItem("department", dept.name.toLowerCase());
+                                    navigate(`/stores/${store.name.toLowerCase()}/${dept.name.toLowerCase()}`);
+                                }}
                             />
                         );
                     })
