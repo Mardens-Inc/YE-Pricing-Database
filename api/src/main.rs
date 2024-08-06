f033759a1719431b5c59f84166719943fe6d9835
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-06 09:30:21 -0400

    Add 'insert' service to HttpServer
    Updated the dependencies and added the 'insert' service in the actix web server setup. This allows the server to handle both 'range' and 'insert' HTTP services, bolstering its functionality.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,6 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::{web, App, HttpServer};
 
-use httaccess::range;
+use httaccess::{insert, range};
 
 mod database_config;
 mod database_row_entry;
@@ -8,28 +8,29 @@ mod db_access;
 mod httaccess;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	let config = get_config().await;
-	// Start the server
+    let config = get_config().await;
+    // Start the server
 
-	println!("Server running at http://127.0.0.1:1870/");
-	HttpServer::new(move || {
-		App::new()
-			.app_data(web::Data::new(config.clone()))
-			.service(range)
-	})
-		.bind(("127.0.0.1", 1870))?
-		.run()
-		.await
+    println!("Server running at http://127.0.0.1:1870/");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(config.clone()))
+            .service(range)
+            .service(insert)
+    })
+    .bind(("127.0.0.1", 1870))?
+    .run()
+    .await
 }
 
 async fn get_config() -> database_config::DatabaseConfig {
-	let client = reqwest::Client::new();
-	let res = client
-		.get("https://lib.mardens.com/config.json")
-		.send()
-		.await
-		.unwrap();
-	let body = res.text().await.unwrap();
-	let config: database_config::DatabaseConfig = serde_json::from_str(&body).unwrap();
-	config
+    let client = reqwest::Client::new();
+    let res = client
+        .get("https://lib.mardens.com/config.json")
+        .send()
+        .await
+        .unwrap();
+    let body = res.text().await.unwrap();
+    let config: database_config::DatabaseConfig = serde_json::from_str(&body).unwrap();
+    config
 }
