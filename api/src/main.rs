69dd1380dc444cc489a80f446798272f70718aba
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-06 08:48:49 -0400

    Add dynamic server configuration fetch
    Fetch server configuration dynamically from a remote URL and pass it to the Actix web application. This includes printing the server start message and integrating `database_config` for configuration management.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,35 @@
+use actix_web::{App, HttpServer, web};
+
+use httaccess::range;
+
+mod database_config;
 mod database_row_entry;
 mod db_access;
 mod httaccess;
-
-use actix_web::{App, HttpServer};
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	HttpServer::new(|| {
+	let config = get_config().await;
+	// Start the server
+
+	println!("Server running at http://127.0.0.1:1870/");
+	HttpServer::new(move || {
 		App::new()
+			.app_data(web::Data::new(config.clone()))
+			.service(range)
 	})
 		.bind(("127.0.0.1", 1870))?
 		.run()
 		.await
+}
+
+async fn get_config() -> database_config::DatabaseConfig {
+	let client = reqwest::Client::new();
+	let res = client
+		.get("https://lib.mardens.com/config.json")
+		.send()
+		.await
+		.unwrap();
+	let body = res.text().await.unwrap();
+	let config: database_config::DatabaseConfig = serde_json::from_str(&body).unwrap();
+	config
 }
