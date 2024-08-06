ff0228b9d4b6f722753c99a7fdba86bfa90223c2
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-06 08:48:23 -0400

    Add structs for database row and insertion handling
    Introduced `DatabaseRowEntry`, `DatabaseInsertEntry`, and `DBResult` structs for managing database rows, insertion data, and paginated results, respectively. This addition will streamline data handling and serialization, facilitating smoother database operations.

---
+++ api/src/database_row_entry.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DatabaseRowEntry {
+    pub id: i32,
+    pub tag_number: i32,
+    pub store: i32,
+    pub department: i32,
+    pub percent: f32,
+    pub mardens_price: f32,
+    pub quantity: i32,
+    pub description: String,
+    pub employee: i32,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DatabaseInsertEntry {
+    pub tag_number: i32,
+    pub store: i32,
+    pub department: i32,
+    pub percent: f32,
+    pub mardens_price: f32,
+    pub quantity: i32,
+    pub description: String,
+    pub employee: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DBResult {
+    pub data: Vec<DatabaseRowEntry>,
+    pub per_page: i32,
+    pub current_page: i32,
+    pub last_page: i32,
+    pub count: i32,
+    pub total: i32,
+}
