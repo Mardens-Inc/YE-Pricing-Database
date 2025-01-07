5966d0d13ca60186893837fee00d31c39aa309d7
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-07 15:55:32 -0500

    Add legacy plugin to Vite config (commented out)
    Introduced the legacy plugin in the Vite configuration, though it's currently commented out. This addition prepares for potential support of older browser targets if needed in the future. Existing configurations remain unchanged.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {legalComments: 'none'},
-  server:{
-    port: 2024
-  }
-})
+    plugins: [react(),
+        // legacy({
+        //     targets: ["defaults", "not IE 11", "Chrome >= 49"], // Specify browser versions
+        //     additionalLegacyPolyfills: ["regenerator-runtime/runtime"] // To support async/await
+        // })
+    ],
+    esbuild: {legalComments: "none"},
+    server: {
+        port: 2024
+    }
+});
 
