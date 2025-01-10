f0de57b2deeadd45363d8e2bce9725e2af9e5a47
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-10 09:03:36 -0500

    Remove legacy plugin and define environment variable
    The legacy plugin was commented out and subsequently removed to streamline the configuration. Added a `process.env.NODE_ENV` definition for environment-specific behavior. These changes simplify the Vite setup and enhance clarity.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(),
-        // legacy({
-        //     targets: ["defaults", "not IE 11", "Chrome >= 49"], // Specify browser versions
-        //     additionalLegacyPolyfills: ["regenerator-runtime/runtime"] // To support async/await
-        // })
-    ],
+    plugins: [react()],
     esbuild: {legalComments: "none"},
+    define: {
+        "process.env.NODE_ENV": JSON.stringify("development") // or "production", depending on your environment
+    },
     server: {
         port: 2024
     }
